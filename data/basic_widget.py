1e5be2909b5f9036603079264df526e5fa2b3bad
Alex Karavan <alek.karavan@gmail.com>
2023-12-29 23:37:09 +0100

    Переработка виджетов

---
+++ data/basic_widget.py
@@ -0,0 +1,19 @@
+import datetime
+from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
+from main import Notifications
+
+class Based_Widget:
+    def __init__(self, *args, **kwargs):
+        self.lay = AnchorLayout()
+
+    def show(self):
+        return self.lay
+
+    def close(self):
+        self.lay.parent.remove_widget(self.lay)
--- data/document.py
+++ data/document.py
@@ -4,24 +4,24 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.image import Image
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 from main import Notifications
 from data.basic_widget import Based_Widget
 
-class Notification(Based_Widget):
-    def __init__(self, type='info', index=0, tytle='', text='', date=datetime.datetime):
+class Documents(Based_Widget):
+    def __init__(self, type='info', tytle='', text='', image=''):
         super().__init__()
         self.type = type
-        self.index = index
         self.tytle = tytle
         self.text = text
-        self.date = date
+        self.image = image
         tytle = Label(text=self.tytle)
         text = Label(text=self.text)
-        date = Label(text=self.date)
+        image = Image(image=self.image)
         close = Button(text='close', on_press=self.close)
-        #тип как то будет влиять на внешку
         self.lay.add_widget(tytle)
         self.lay.add_widget(text)
+        self.lay.add_widget(image)
         self.lay.add_widget(close)
